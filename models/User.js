cd375f52ad496e3bf5c10659b0fa4db8f2e92a8a
gabriel-dias-clear <gabriel.dias.clear@gmail.com>
2023-08-07 08:44:10 -0300

    add user class (add POO)

---
+++ models/User.js
@@ -0,0 +1,20 @@
+class User {
+
+    constructor(name, gender, birth, country, email, pwd, photo, admin){
+
+        // *variaveis dentro de uma classe é chamado de atributo ou propriedade do objeto*
+
+        this.name = name;
+//nome da propriedade do obj / nome do parametro passado no construtor
+        this.gender = gender;
+        this.birth = birth;
+        this.country = country;
+        this.email = email;
+        this.pwd = pwd;
+        this.photo = photo;
+        this.admin = admin;
+
+
+    }
+
+}
